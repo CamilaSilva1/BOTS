b3bec8401986ad1fdf8a955be7454f03a2aac28e
Camila Silva <millamanson1234@gmail.com>
2022-03-04 16:06:01 -0300

    chatBots em Python

---
+++ instagramBot.py
@@ -0,0 +1,60 @@
+#importar as bibliotecas
+from selenium import webdriver
+import time
+from webdriver_manager import driver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+class instagramBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        
+
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com')  
+        time.sleep(2)     
+        campo_login = driver.find_element_by_xpath("//input[@name='username']")
+        campo_login.send_keys(self.username)
+
+        campo_login = driver.find_element_by_xpath("//input[@name='password']")
+        campo_login.send_keys(self.password)
+        time.sleep(2)
+        campo_login.send_keys(Keys.RETURN)
+        time.sleep(4)
+
+        salvar_info = driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button")
+        salvar_info.click()
+        time.sleep(4)
+
+        salvar_info = driver.find_element_by_xpath(" /html/body/div[4]/div/div/div/div[3]/button[2]")
+        salvar_info.click()
+        self.procurar_perfil('camila_da_silva_assis')
+        
+
+    def procurar_perfil(self, nome):
+        driver = self.driver
+        driver.get('https://www.instagram.com/' + nome + '/')
+        
+        
+       
+ 
+camilaBot = instagramBot('camila1234@gmail.com', '123')
+camilaBot.login()
+
+
+#daniieel_castro10
+
+
+   
+   
+  
+
+
+
+
+
+
+
---
+++ whatsappbot.py
@@ -0,0 +1,39 @@
+#importar as bibliotecas
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+#navegar ate o whatsapp web
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.whatsapp.com')
+time.sleep(30)
+
+#definir contatos e grupos e mensagens a serem enviadas
+contatos = ['Grupo eu']
+mensagem = 'Olá?'
+
+#buscar contatos ou grupos
+def buscar_contato(contato):
+   campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+   time.sleep(3)
+   campo_pesquisa.click()
+   campo_pesquisa.send_keys(contato)
+   campo_pesquisa.send_keys(Keys.ENTER)
+
+#enviar mensagens pra eles
+def enviar_mensagem(mensagem):
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    campo_mensagem[1].click()
+    time.sleep(4)
+    campo_mensagem[1].send_keys(mensagem)
+    time.sleep(4)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+for contato in contatos:
+    buscar_contato(contato)
+    enviar_mensagem(mensagem)
+
+#campo de pesquisa  'copyable-text selectable-text'
+#campo de mensagem privada  'copyable-text selectable-text'
+
